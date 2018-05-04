5b36295f500d9faf4cff3f5ceea4d8a89635cafa
Rudya93 <rudyaa93@gmail.com>
2018-05-04 12:28:24 +0300

    boto

---
+++ boto-examples/aws_5_session_low_level_client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import boto3
+import json
+import pprint
+
+session = boto3.Session()
+
+ec2c = session.client('ec2')
+
+zones = ec2c.describe_availability_zones()
+pprint.pprint(zones, indent=2)
+
+instances = ec2c.describe_instances()
+pprint.pprint(instances, indent=2)
